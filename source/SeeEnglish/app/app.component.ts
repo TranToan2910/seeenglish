c995ddd69585989686966b832cc4d7ecf5b9fac5
TranToan2910 <shiro2910@gmail.com>
2018-03-10 23:00:08 +0700

    add hello world and set up visual code ignore native script schema error

--- source/SeeEnglish/app/app.component.ts
+++ source/SeeEnglish/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  selector: 'my-app',
+  template: `<Label text="hello world!"></Label>`
 })
 export class AppComponent {}
--- source/SeeEnglish/app/app.module.ts
+++ source/SeeEnglish/app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NgModule } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+// disable angular warning for native script UI element
+import { NO_ERRORS_SCHEMA } from '@angular/core'
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+// import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   imports: [NativeScriptModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule {}
